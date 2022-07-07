47a66d28b38047b242a9dbab2107e63f615fbbfd
oscar.forss <oscar.forss@globalconnect.se>
2022-07-07 09:50:48 +0200

    clean code

--- src/main/java/io/wakelesstuna/jpaprojectiondemo/repositories/projections/PersonView.java
+++ src/main/java/io/wakelesstuna/jpaprojectiondemo/repositories/projections/PersonView.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 
 public interface PersonView {
 
+    /* SpEL expression */
     @Value("#{target.firstName + ' ' + target.lastName}")
     String getFullName();
     AddressView getAddress();
